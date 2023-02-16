78fc36c62b142815732d7e5f013f6def328f3abf
Sam <asamadans@gmail.com>
2023-02-16 19:06:41 +0530

    initial commit

---
+++ flights/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class AIrport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city}: {self.code}"
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination} in {self.duration} minutes"
