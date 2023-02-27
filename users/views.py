7e90c17e00e9b3f80dfa104badde0395ad3241cc
Sam <asamadans@gmail.com>
2023-02-27 20:24:55 +0530

    completed

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
-    return render(request, "index.html")
+    return render(request, "user.html")
     
 def login_view(request):
     if request.method == "POST":
